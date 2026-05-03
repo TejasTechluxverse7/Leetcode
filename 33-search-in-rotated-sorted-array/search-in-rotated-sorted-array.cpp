c95834c35a9f072e2db2570a79c87048f0271202
Tejas Vilas Kondhalkar <tejasvilas04@gmail.com>
2026-05-03 09:21:22 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (34.09%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            // Check if left half is sorted
+            if (nums[low] <= nums[mid]) {
+                // Target lies in left half
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // Otherwise, right half is sorted
+            else {
+                // Target lies in right half
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
